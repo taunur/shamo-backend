d8dcade7dcdc911fe0324308ac69db93dd0a8d9d
Taufik Nurrahman <official.taufik980@gmail.com>
2023-08-12 09:07:29 +0700

    API Login

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,11 +7,13 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
+    // register
     public function register(Request $request)
     {
         try {
@@ -42,6 +44,42 @@ class UserController extends Controller
                 'token_type'   => 'Bearer',
                 'user'         => $user
             ], 'User sudah terdaftar');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error'   => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    // login
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email'    => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type'   => 'Bearer',
+                'user'         => $user
+            ], 'Authenticated');
         } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
