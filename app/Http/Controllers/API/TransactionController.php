e2da9326fab2ae399ca01752e68ab73870f3f9e4
Taufik Nurrahman <official.taufik980@gmail.com>
2023-08-12 10:17:25 +0700

    API Transcation

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id     = $request->input('id');
+        $limit  = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['items.product'])->find($id);
+
+            if ($transaction) {
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data Berhasil Diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data transaksi tidak ada',
+                    404
+                );
+            }
+        }
+
+        // status
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        // limit
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list transaksi berhasil diambil'
+        );
+    }
+}
