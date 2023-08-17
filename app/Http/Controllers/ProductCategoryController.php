dbff3238b06a1b5ae04e77cde0655eb9f1ac9fb7
Taufik Nurrahman <official.taufik980@gmail.com>
2023-08-17 12:31:47 +0700

    add dashboard web

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductCategoryRequest;
+use App\Models\ProductCategory;
+use Yajra\DataTables\Facades\DataTables;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = ProductCategory::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                    <a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                        href="' . route('dashboard.category.edit', $item->id) . '">
+                        Edit
+                    </a>
+                    <form class="inline-block" action="' . route('dashboard.category.destroy', $item->id) . '" method="POST">
+                    <button class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-1 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                        Hapus
+                    </button>
+                        ' . method_field('delete') . csrf_field() . '
+                    </form>';
+                })
+                ->editColumn('price', function ($item) {
+                    return number_format($item->price);
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('pages.dashboard.category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.dashboard.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductCategoryRequest $request)
+    {
+        $data = $request->all();
+
+        ProductCategory::create($data);
+
+        return redirect()->route('dashboard.category.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductCategory $category)
+    {
+        return view('pages.dashboard.category.edit', [
+            'item' => $category
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductCategoryRequest $request, ProductCategory $category)
+    {
+        $data = $request->all();
+
+        $category->update($data);
+
+        return redirect()->route('dashboard.category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductCategory $category)
+    {
+        $category->delete();
+
+        return redirect()->route('dashboard.category.index');
+    }
+}
